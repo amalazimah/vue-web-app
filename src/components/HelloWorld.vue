09e4b642c1d1e7042ee5630fe9b61e69d017eba9
amalazimah <19FTT1205@student.pb.edu.bn>
2021-03-30 12:52:25 +0800

    install cssnano for purging

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
     <figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-  <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
+  <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="../assets/zimzalabim.jpg" alt="" width="384" height="512">
   <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p class="text-lg font-semibold">
@@ -9,10 +9,10 @@
     </blockquote>
     <figcaption class="font-medium">
       <div class="text-cyan-600">
-        Sarah Dayan
+        youtube videos, moodboarding and sleep
       </div>
       <div class="text-gray-500">
-        Staff Engineer, Algolia
+        19FTT1205, DWDT05
       </div>
     </figcaption>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.css',
+    './src/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
